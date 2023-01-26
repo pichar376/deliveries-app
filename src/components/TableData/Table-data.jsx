39ba9cc78437b8a143b1a3169b2519adbd990619
pichar376 <pichar376@gmail.com>
2023-01-26 00:20:40 -0600

    search render modified

--- src/components/TableData/Table-data.jsx
+++ src/components/TableData/Table-data.jsx
@@ -13,6 +13,7 @@ import { BsLayoutTextSidebar } from "react-icons/bs";
 import { dataStyle, labelStyle, tableCellStyles } from "./table.styles";
 import ButtonSecondary from "../ButtonSecondary/ButtonSecondary";
 import { useNavigate } from "react-router-dom";
+import Search from "../Search/Search";
 
 const TableData = ({ deliveryHistory, setDeliveryHistory }) => {
   const navigate = useNavigate();
@@ -22,87 +23,93 @@ const TableData = ({ deliveryHistory, setDeliveryHistory }) => {
   };
 
   return (
-    <TableContainer sx={{ mt: "50px" }}>
-      <Table>
-        <TableBody>
-          {deliveryHistory.map((row, index) => (
-            <TableRow
-              key={index}
-              sx={{
-                height: "104px",
-                "&&.MuiTableRow-root": {
-                  ...(row.status === "Pending" && {
-                    borderBottom: "2px solid #307460",
-                  }),
-                },
-              }}
-            >
-              <TableCell sx={{ ...tableCellStyles, width: 90 }}>
-                <Box>
-                  <Typography sx={labelStyle}>Status</Typography>
-                  <Typography sx={dataStyle}>{row.status}</Typography>
-                </Box>
-              </TableCell>
-              <TableCell sx={{ ...tableCellStyles, width: 105 }}>
-                <Box>
-                  <Typography sx={labelStyle}>OrderId</Typography>
-                  <Typography sx={{ ...dataStyle, width: "7rem" }}>
-                    {row.orderId}
-                  </Typography>
-                </Box>
-              </TableCell>
-              <TableCell sx={{ ...tableCellStyles, width: 124 }}>
-                <Box>
-                  <Typography sx={labelStyle}>Technician</Typography>
-                  <Typography sx={{ ...dataStyle, width: "8rem" }}>
-                    {row.technician}
-                  </Typography>
-                </Box>
-              </TableCell>
-              <TableCell sx={{ ...tableCellStyles, width: 70 }}>
-                <Box>
-                  <Typography sx={labelStyle}>Platform</Typography>
-                  <Typography sx={dataStyle}>{row.platform}</Typography>
-                </Box>
-              </TableCell>
-              <TableCell sx={{ ...tableCellStyles, width: 105 }}>
-                <Box>
-                  <Typography sx={labelStyle}>Drone</Typography>
-                  <Typography sx={dataStyle}>{row.drone}</Typography>
-                </Box>
-              </TableCell>
-              <TableCell sx={{ ...tableCellStyles, width: 124 }}>
-                <Box>
-                  <Typography sx={{ ...labelStyle, width: "7rem" }}>
-                    Technical check
-                  </Typography>
-                  <Typography sx={dataStyle}>{row.technicalCheck}</Typography>
-                </Box>
-              </TableCell>
+    <>
+      <Search
+        deliveryHistory={deliveryHistory}
+        setDeliveryHistory={setDeliveryHistory}
+      />
+      <TableContainer sx={{ mt: "50px" }}>
+        <Table>
+          <TableBody>
+            {deliveryHistory.map((row, index) => (
+              <TableRow
+                key={index}
+                sx={{
+                  height: "104px",
+                  "&&.MuiTableRow-root": {
+                    ...(row.status === "Pending" && {
+                      borderBottom: "2px solid #307460",
+                    }),
+                  },
+                }}
+              >
+                <TableCell sx={{ ...tableCellStyles, width: 90 }}>
+                  <Box>
+                    <Typography sx={labelStyle}>Status</Typography>
+                    <Typography sx={dataStyle}>{row.status}</Typography>
+                  </Box>
+                </TableCell>
+                <TableCell sx={{ ...tableCellStyles, width: 105 }}>
+                  <Box>
+                    <Typography sx={labelStyle}>OrderId</Typography>
+                    <Typography sx={{ ...dataStyle, width: "7rem" }}>
+                      {row.orderId}
+                    </Typography>
+                  </Box>
+                </TableCell>
+                <TableCell sx={{ ...tableCellStyles, width: 124 }}>
+                  <Box>
+                    <Typography sx={labelStyle}>Technician</Typography>
+                    <Typography sx={{ ...dataStyle, width: "8rem" }}>
+                      {row.technician}
+                    </Typography>
+                  </Box>
+                </TableCell>
+                <TableCell sx={{ ...tableCellStyles, width: 70 }}>
+                  <Box>
+                    <Typography sx={labelStyle}>Platform</Typography>
+                    <Typography sx={dataStyle}>{row.platform}</Typography>
+                  </Box>
+                </TableCell>
+                <TableCell sx={{ ...tableCellStyles, width: 105 }}>
+                  <Box>
+                    <Typography sx={labelStyle}>Drone</Typography>
+                    <Typography sx={dataStyle}>{row.drone}</Typography>
+                  </Box>
+                </TableCell>
+                <TableCell sx={{ ...tableCellStyles, width: 124 }}>
+                  <Box>
+                    <Typography sx={{ ...labelStyle, width: "7rem" }}>
+                      Technical check
+                    </Typography>
+                    <Typography sx={dataStyle}>{row.technicalCheck}</Typography>
+                  </Box>
+                </TableCell>
 
-              <TableCell sx={{ padding: "16px 0px 16px 16px" }}>
-                <Stack direction="row" sx={{ gap: "16px" }}>
-                  <Box mr="auto" />
-                  <ButtonSecondary
-                    Icon={BsLayoutTextSidebar}
-                    onClick={() => {
-                      findRow(row.orderId);
-                    }}
-                  >
-                    Details
-                  </ButtonSecondary>
-                  <MenuDelivery
-                    orderId={row.orderId}
-                    deliveryHistory={deliveryHistory}
-                    setDeliveryHistory={setDeliveryHistory}
-                  />
-                </Stack>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                <TableCell sx={{ padding: "16px 0px 16px 16px" }}>
+                  <Stack direction="row" sx={{ gap: "16px" }}>
+                    <Box mr="auto" />
+                    <ButtonSecondary
+                      Icon={BsLayoutTextSidebar}
+                      onClick={() => {
+                        findRow(row.orderId);
+                      }}
+                    >
+                      Details
+                    </ButtonSecondary>
+                    <MenuDelivery
+                      orderId={row.orderId}
+                      deliveryHistory={deliveryHistory}
+                      setDeliveryHistory={setDeliveryHistory}
+                    />
+                  </Stack>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
